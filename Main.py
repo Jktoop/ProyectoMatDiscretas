0c0b863ed1011dca350186da2a7dda446af8584b
Jktoop <jcarrillo2026@alu.uct.cl>
2026-06-20 20:16:40 -0400

    importaciondelibrerias

---
+++ Main.py
@@ -0,0 +1,8 @@
+import tkinter as tk 
+from tkinter import ttk, massagebox
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import heapq
+
