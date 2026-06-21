df88ea54e9159f41894dd650ad97873d08afe0f3
Kevvyh <kvn.inalaf@proton.me>
2026-06-20 21:10:44 -0400

    prueba papus

--- Main.py
+++ Main.py
@@ -3,3 +3,5 @@ from tkinter import ttk, messagebox
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+print("hola papus")                   
